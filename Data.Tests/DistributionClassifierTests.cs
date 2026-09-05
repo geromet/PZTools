f1c7d518c8180162448f74f737b56ec1f67d50c8
geromet <81250489+geromet@users.noreply.github.com>
2026-09-05 09:16:27 +0200

    Cover structural item-family classifier rules

--- Data.Tests/DistributionClassifierTests.cs
+++ Data.Tests/DistributionClassifierTests.cs
@@ -9,14 +9,30 @@ public sealed class DistributionClassifierTests
     [Theory]
     [InlineData("Bag_FutureBuildContainer")]
     [InlineData("bag_futurebuildcontainer")]
-    [InlineData("Bag_ProtectiveCaseNextBuild")]
-    public void BagPrefix_ClassifiesPreviouslyUnknownNamesAsItems(string name)
+    [InlineData("AmmoStrap_FutureCalibre")]
+    [InlineData("CookieJar_FutureShape")]
+    [InlineData("Cooler_FutureContents")]
+    [InlineData("FirstAidKit_FutureTier")]
+    [InlineData("HollowBook_FutureLoot")]
+    [InlineData("JewelleryBox_FutureStyle")]
+    [InlineData("KeyRing_FutureCharm")]
+    [InlineData("Parcel_FutureSize")]
+    [InlineData("PhotoAlbum_FutureAge")]
+    [InlineData("Present_FutureSize")]
+    [InlineData("TakeoutBox_FutureMaterial")]
+    [InlineData("Toolbox_FutureTrade")]
+    [InlineData("ToolRoll_FutureMaterial")]
+    [InlineData("Wallet_FutureStyle")]
+    public void StructuralItemPrefixes_ClassifyFutureVariantsAsItems(string name)
     {
         Assert.Equal(DistributionType.Item, DistributionClassifier.Classify(name));
     }
 
     [Theory]
     [InlineData("Cashbox", DistributionType.Item)]
+    [InlineData("Toolbox", DistributionType.Item)]
+    [InlineData("Cooler", DistributionType.Item)]
+    [InlineData("FirstAidKit", DistributionType.Item)]
     [InlineData("FoodCache1", DistributionType.Cache)]
     [InlineData("Carpenter", DistributionType.Profession)]
     [InlineData("Kitchen", DistributionType.Room)]
@@ -31,6 +47,14 @@ public sealed class DistributionClassifierTests
     [InlineData("BagRoom")]
     [InlineData("BaggageClaim")]
     [InlineData("Handbag_Unknown")]
+    [InlineData("AmmoStrapRoom")]
+    [InlineData("CoolerRoom")]
+    [InlineData("FirstAidKitRoom")]
+    [InlineData("KeyRingRoad")]
+    [InlineData("ParcelRoom")]
+    [InlineData("PresentRoom")]
+    [InlineData("ToolboxRoom")]
+    [InlineData("WalletRoom")]
     public void SimilarButNonStructuralNamesStillFallBackToRoom(string name)
     {
         Assert.Equal(DistributionType.Room, DistributionClassifier.Classify(name));
